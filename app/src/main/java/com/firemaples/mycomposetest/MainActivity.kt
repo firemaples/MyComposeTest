930771d31353fa226736e751a88fe7a4d8d53393
firemaples <firemaples@gmail.com>
2022-02-05 23:43:33 +0800

    temp

--- app/src/main/java/com/firemaples/mycomposetest/MainActivity.kt
+++ app/src/main/java/com/firemaples/mycomposetest/MainActivity.kt
@@ -3,37 +3,84 @@ package com.firemaples.mycomposetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent()
-
-//            MyComposeTestTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(color = MaterialTheme.colors.background) {
-//                    Greeting("Android")
-//                }
-//            }
+            Greetings()
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun MainContent() {
-    Text("New text")
-    Text("Line 2")
+fun FullPreview() {
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnBoarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnBoarding = !shouldShowOnBoarding })
+    } else {
+        Greetings()
+    }
 }
 
+@Preview(showBackground = true, name = "Greetings", widthDp = 320)
+@Composable
+fun MainContent() {
+    Greetings()
+}
+
+//@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES, name = "Night")
 //@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Hello $name!")
+//fun MainContentDark() {
+//    App()
 //}
+
+@Composable
+fun Greetings(names: List<String> = List(1000) { "#$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        val expended = remember { mutableStateOf(false) }
+        val extraPadding = if (expended.value) 48.dp else 0.dp
+
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+
+            OutlinedButton(onClick = { expended.value = !expended.value }) {
+                Text(text = if (expended.value) "Show less" else "Show more")
+            }
+        }
+
+    }
+}
 //
 //@Preview(showBackground = true)
 //@Composable
@@ -42,3 +89,32 @@ fun MainContent() {
 //        Greeting("Android")
 //    }
 //}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    MaterialTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+
